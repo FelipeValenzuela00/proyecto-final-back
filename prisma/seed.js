d38be1063334c478f83d46e85e0d78f2bb86e254
Felipe Valenzuela <feelipevalenzuela@hotmail.com>
2026-04-29 16:35:01 -0300

    integracion merge de develop

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const prisma = require('../src/shared/database/prisma');
 
 async function main() {
     console.log('🌱 Iniciando el proceso de seeding...');
